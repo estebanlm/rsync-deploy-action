eb523c755ed94d06cd384be6965c35acf417678a
Esteban Lorenzano <estebanlm@netc.eu>
2022-12-15 15:07:16 +0100

    Restore defaults. Force ipv6 to see what happens.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,6 @@ mkdir -p /root/.ssh/
 echo "${INPUT_PRIVATE_SSH_KEY}" > /root/.ssh/id_rsa.key
 chmod 600 /root/.ssh/id_rsa.key
 
-INPUT_HOST=files.pharo.org
-INPUT_PORT=22
-INPUT_USER=pharoorgde
-
 ssh-keyscan -H ${INPUT_HOST} >> /root/.ssh/known_hosts
 cat >>/root/.ssh/config <<END
 Host server
@@ -18,14 +14,12 @@ Host server
     IdentityFile /root/.ssh/id_rsa.key
 END
 
-cmd_rsync="rsync -avz"
+cmd_rsync="rsync -avz -6"
 
 # Uploading the file-s
 echo Upload file/-s...
-${cmd_rsync} ${GITHUB_WORKSPACE}/generated files.pharo.org:/homez.141/pharoorgde/www
-
-#if [ ${INPUT_REPOSITORY_PATH:0:1} = "/" ]; then
-#  ${cmd_rsync} ${GITHUB_WORKSPACE}/${INPUT_REPOSITORY_PATH:1} ${INPUT_HOST}:${INPUT_SERVER_PATH}
-#else
-#  ${cmd_rsync} ${GITHUB_WORKSPACE}/${INPUT_REPOSITORY_PATH} ${INPUT_HOST}:${INPUT_SERVER_PATH}
-#fi
+if [ ${INPUT_REPOSITORY_PATH:0:1} = "/" ]; then
+  ${cmd_rsync} ${GITHUB_WORKSPACE}/${INPUT_REPOSITORY_PATH:1} ${INPUT_HOST}:${INPUT_SERVER_PATH}
+else
+  ${cmd_rsync} ${GITHUB_WORKSPACE}/${INPUT_REPOSITORY_PATH} ${INPUT_HOST}:${INPUT_SERVER_PATH}
+fi
