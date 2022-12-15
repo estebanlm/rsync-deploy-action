b1fded1477de32fcfd88d6212418f3396a7ee320
Esteban Lorenzano <estebanlm@netc.eu>
2022-12-15 13:27:51 +0100

    hardcoding some values

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,11 @@
 mkdir -p /root/.ssh/
 echo "${INPUT_PRIVATE_SSH_KEY}" > /root/.ssh/id_rsa.key
 chmod 600 /root/.ssh/id_rsa.key
+
+INPUT_HOST=files.pharo.org
+INPUT_PORT=22
+INPUT_USER=pharoorgde
+
 ssh-keyscan -H ${INPUT_HOST} >> /root/.ssh/known_hosts
 cat >>/root/.ssh/config <<END
 Host server
@@ -15,17 +20,12 @@ END
 
 cmd_rsync="rsync -avz"
 
-echo ">>>"
-echo ${INPUT_REPOSITORY_PATH}
-echo ${INPUT_SERVER_PATH}
-echo ${INPUT_HOST}
-echo ${GITHUB_WORKSPACE}
-echo "<<<"
-
 # Uploading the file-s
 echo Upload file/-s...
-if [ ${INPUT_REPOSITORY_PATH:0:1} = "/" ]; then
-  ${cmd_rsync} ${GITHUB_WORKSPACE}/${INPUT_REPOSITORY_PATH:1} ${INPUT_HOST}:${INPUT_SERVER_PATH}
-else
-  ${cmd_rsync} ${GITHUB_WORKSPACE}/${INPUT_REPOSITORY_PATH} ${INPUT_HOST}:${INPUT_SERVER_PATH}
-fi
+${cmd_rsync} ${GITHUB_WORKSPACE}/generated files.pharo.org:/homez.141/pharoorgde/www
+
+#if [ ${INPUT_REPOSITORY_PATH:0:1} = "/" ]; then
+#  ${cmd_rsync} ${GITHUB_WORKSPACE}/${INPUT_REPOSITORY_PATH:1} ${INPUT_HOST}:${INPUT_SERVER_PATH}
+#else
+#  ${cmd_rsync} ${GITHUB_WORKSPACE}/${INPUT_REPOSITORY_PATH} ${INPUT_HOST}:${INPUT_SERVER_PATH}
+#fi
